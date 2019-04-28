2f9266315e80ec712113f2eb5dc5e281c14889ce
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2019-04-28 20:15:37 +0300

    Added Unity project

---
+++ Ludum Dare 44/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    public SoundEffect[] sounds;
+
+    private AudioSource source;
+
+    public static SoundManager instance;
+    
+    private void Awake()
+    {
+        instance = this;
+        
+        source = GetComponent<AudioSource>();
+    }
+
+    public void Play(string soundName)
+    {
+        SoundEffect sound = sounds.First(s => s.name == soundName);
+        
+        source.PlayOneShot(sound.clip, sound.volume);
+    }
+}
+
+[System.Serializable]
+public struct SoundEffect
+{
+    public AudioClip clip;
+    public float volume;
+    public string name;
+}
